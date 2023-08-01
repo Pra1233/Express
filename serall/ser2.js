96737a7502d0752294b6593a37ea2e5e2af34e26
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-08-01 13:32:26 +0530

    Done

--- serall/ser2.js
+++ serall/ser2.js
@@ -4,10 +4,9 @@ const app=express();
 
 app.use(bodyParser.urlencoded({extended:false})); //it  will pass body to all middleware
 
-app.use(admin);
-app.use(shop);
+
 app.use('/addproduct' ,(req,res,next)=>{
-res.send('<html> <form action="/product" method="POST"> <input type="text" name="k"><button type="submit">SUBMIT</button>  </form>  </html>');
+res.send('<html> <form action="/product" method="POST"> <input type="text" name="k">  <input type="number" name="size">   <button type="submit">SUBMIT</button>  </form>  </html>');
 })
 
 app.post('/product', (req,res)=>{
